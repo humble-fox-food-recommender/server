d2208f43fa226f6e5f2e61f6fd42265dadd16671
andreassosilo <andreas.sosilo@gmail.com>
2019-09-05 21:42:02 +0700

    API currency done

--- controllers/currency.js
+++ controllers/currency.js
@@ -1,9 +1,40 @@
-'use strict'
+'use strict';
+
+// const request = require('request');
+const axios = require('axios')
+const appID = process.env.CURRENCY_TOKEN;
 
 class CurrencyController {
-  static getCurrency() {
+  static getRate(req, res, next) {
+    axios({
+      method: 'get',
+      url: 'https://openexchangerates.org/api/latest.json',
+      headers: {
+        Authorization: `Token ${appID}`
+      }
+    })
+      .then((rates) => {
+        res.status(200).json(Object.keys(rates.data.rates))
+      }).catch(next)
+  }
 
+  static changeRate(req, res, next) {
+    const { price, currency } = req.body
+    axios({
+      method: 'get',
+      url: 'https://openexchangerates.org/api/latest.json',
+      headers: {
+        Authorization: `Token ${appID}`
+      }
+    })
+      .then((rates) => {
+        let foodPrice = price
+        const priceInUSD = Number(foodPrice / rates.data.rates.IDR) // Rupiah ke USD
+        let convertedPrice = (priceInUSD * rates.data.rates[currency]).toFixed(2)  // USD ke mata uang yg dipilih ex.JPY
+        console.log(convertedPrice)
+        res.status(200).json(convertedPrice)
+      }).catch(next)
   }
 }
 
-module.exports = CurrencyController
+module.exports = CurrencyController;
