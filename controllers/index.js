fa1f35e7aebc96c2876c99bc1ba2a0624ceae78c
andreassosilo <andreas.sosilo@gmail.com>
2019-09-06 10:13:58 +0700

    Add Google Sign In routing & controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@
 
 const CurrencyController = require('./currency')
 const ZomatoController = require('./zomato')
+const UserController = require('./user')
 
-module.exports = { CurrencyController, ZomatoController }
+module.exports = { CurrencyController, ZomatoController, UserController }
