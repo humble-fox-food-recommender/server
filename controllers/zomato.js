f2615fe4b64f0bb0ee70003c96f7a364cdfc972f
andreassosilo <andreas.sosilo@gmail.com>
2019-09-06 10:20:39 +0700

    Add Google Sign In & install bcryptjs

--- controllers/zomato.js
+++ controllers/zomato.js
@@ -2,25 +2,26 @@
 
 const axios = require('axios')
 class ZomatoController {
-    static search(req,res,next){
+    static search(req, res, next) {
         console.log("masuk")
-        console.log(req.params.parameter+req.params.page)
+        console.log(req.params.parameter + req.params.page)
         let pagestart = 0
-        if(req.params.page > 0){
-            pagestart = req.params.page*20
-        } 
+        if (req.params.page > 0) {
+            pagestart = req.params.page * 20
+        }
         axios({
             method: 'GET',
             url: `https://developers.zomato.com/api/v2.1/search?q=${req.params.parameter}&start=${pagestart}`,
-            headers:{
-                "user-key" : "ea12d4986a8a3af3d22567b65e4cfe46"
+            headers: {
+                "user-key": "ea12d4986a8a3af3d22567b65e4cfe46"
             }
-          })
-          .then(restos=>{
-              res.status(200).json(restos.data.restaurants)
-          })
-          .catch(next)
-      
+        })
+            .then(restos => {
+                res.status(200).json(restos.data.restaurants)
+            })
+            .catch(next)
+    }
+
     static getDetail(req, res, next) {
         const { id } = req.params
         const startLoc = `hacktiv8`
