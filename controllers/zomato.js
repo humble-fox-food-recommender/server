f4616bc8471582a1c0686f8ceb7c88cd648fb6b2
zakiarsyad <ozikarsyad@gmail.com>
2019-09-06 08:47:43 +0700

    get duration and distance

--- controllers/zomato.js
+++ controllers/zomato.js
@@ -5,6 +5,8 @@ const axios = require('axios')
 class ZomatoController {
     static getDetail(req, res, next) {
         const { id } = req.params
+        const startLoc = `hacktiv8`
+        let location
 
         axios({
             url: `https://developers.zomato.com/api/v2.1/restaurant?res_id=${id}`,
@@ -14,7 +16,18 @@ class ZomatoController {
             }
         })
             .then(({ data }) => {
-                res.status(200).json(data.location)
+                // res.status(200).json(data.location)
+                location = data.location
+
+                return axios({
+                    url: `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${startLoc}&destinations=${location.address}&key=AIzaSyAdv8835rnAxHq2hwdSMLAS1Dt7Mjk2rAo`,
+                    method: `GET`
+                })
+            })
+            .then(({ data }) => {
+                location.distance = data.rows[0].elements[0].distance.text;
+                location.duration = data.rows[0].elements[0].duration.text
+                res.status(200).json(location)
             })
             .catch(err => {
                 console.log(err)
